5ea50fe8434fdbcae90557118b07eb821e2b07e0
Oleksiy Pyltsov <valar999@gmail.com>
2021-05-16 17:22:12 +0300

    Add deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,19 @@
 import "@nomiclabs/hardhat-waffle";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 module.exports = {
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: [process.env.PRIVATEKEY],
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: [process.env.PRIVATEKEY],
+    },
+  },
   solidity: "0.8.4",
 };
