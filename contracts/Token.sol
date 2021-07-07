fb50b5d005ab3314dad5af4992d3a0ba85e32dfd
svinerus <svinerus@gmail.com>
2021-07-07 19:36:20 +0300

    Add OpenSea integration

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,16 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";  // // Use for OpenSea integration
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Token is AccessControl, ERC721URIStorage {
+
+// Use for OpenSea integration
+contract OwnableDelegateProxy {}
+
+contract ProxyRegistry {
+  mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+
+contract Token is AccessControl, ERC721URIStorage, Ownable {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   Counters.Counter private tokenCounter;
+  address proxyRegistryAddress;  // The address of the OpenSea proxy registry
 
-  constructor() ERC721("WePlay", "WPL") {
+
+  constructor(address _proxyRegistryAddress) ERC721("WePlay", "WPL") {
+    proxyRegistryAddress = _proxyRegistryAddress;
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _setupRole(MINTER_ROLE, msg.sender);
   }
@@ -29,4 +42,15 @@ contract Token is AccessControl, ERC721URIStorage {
 
     return id;
   }
+
+
+  // Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+  function isApprovedForAll(address owner, address operator) override public view returns (bool) {
+    ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+    if (address(proxyRegistry.proxies(owner)) == operator) {
+      return true;
+    }
+    return super.isApprovedForAll(owner, operator);
+  }
+
 }
